2c1db590a4d1794160ae3f02b75948a87630834c
darrenfishell <darrenfishell@pm.me>
2025-07-15 21:33:55 -0700

    Create county bridge table and dbt project config

---
+++ dbt_pipeline/models/bronze/census_ppp_county_bridge.sql
@@ -0,0 +1,41 @@
+{{ config(materialized='table') }}
+
+SELECT DISTINCT
+    s.state_name,
+    p.project_county_name,
+    c.ctyname as census_county_name
+FROM {{ source('ppp_loan_analysis', 'paycheck_protection_loans') }} p
+LEFT JOIN {{ source('census_bureau', 'state_crosswalk') }} s
+ON
+    p.project_state = s.stusab
+LEFT JOIN {{ source('census_bureau', 'census_2020_estimates') }} c
+ON
+    s.state_name = c.stname
+        AND LOWER(
+                REGEXP_REPLACE(
+                    REGEXP_REPLACE(
+                        CASE
+                            WHEN p.project_state = 'VA'
+                            AND p.project_county_name IN ('BRISTOL', 'RADFORD', 'EMPORIA', 'SALEM')
+                                THEN p.project_county_name || ' city'
+                            WHEN p.project_state = 'SD'
+                            AND p.project_county_name = 'PINE RIDGE'
+                                THEN 'OGLALA LAKOTA'
+                            WHEN p.project_county_name = 'DONA ANA'
+                                THEN 'Doña Ana'
+                            ELSE p.project_county_name
+                        END,
+                        '^(ST |SAINTE)', 'SAINT', 'i'),
+                '[ -]|''', '', 'g')
+            ) =
+        LOWER(
+            REGEXP_REPLACE(
+                REGEXP_REPLACE(
+                    REGEXP_REPLACE(c.ctyname, ' (County|Parish|City and Borough|Borough|Municipality|Census Area)', '', 'i'),
+                '(St\. |Ste\. |^St )', 'SAINT', 'i'),
+            '[ -]|''', '', 'g')
+        )
+WHERE
+    c.ctyname IS NOT NULL
+    AND p.project_county_name IS NOT NULL
+    AND p.project_state NOT IN ('PR', 'MP', 'AE', 'VI', 'AS', 'GU')
