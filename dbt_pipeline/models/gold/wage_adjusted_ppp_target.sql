a605fd35da7dffee9cb3526e789cb80bae410c59
darrenfishell <darrenfishell@pm.me>
2025-07-22 00:40:42 -0700

    Add final design matrix for wage-adjusted ppp dollars

---
+++ dbt_pipeline/models/gold/wage_adjusted_ppp_target.sql
@@ -0,0 +1,21 @@
+SELECT
+	b.census_county_name AS county,
+	b.state_name as state,
+	pp.current_approval_amount as ppp_loan_amount,
+	q.total_annual_wages as total_annual_wages,
+	pp.current_approval_amount / q.total_annual_wages as wage_adjusted_ppp_loan_amount,
+	c.white_population / c.total_population as white_pop_share,
+	c.black_population / c.total_population as black_pop_share,
+	(c.native_american_population + c.native_hawaiian_pacific_islander_population) / c.total_population as native_pop_share,
+	c.hispanic_population / c.total_population as hispanic_share,
+	e.republican / e.total as trump_share
+FROM silver.county_level_ppp_loans pp
+JOIN bronze.census_ppp_county_bridge b
+ON pp.project_county_name = b.project_county_name
+AND pp.state_name = b.state_name
+JOIN silver.county_census_2019 c
+ON b.area_fips = c.area_fips
+JOIN silver.county_qcew_summary q
+ON c.area_fips = q.area_fips --3139
+JOIN silver.county_2016_election_results e
+ON b.area_fips = e.area_fips
