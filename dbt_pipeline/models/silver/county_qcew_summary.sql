7cad12bccd9eea645a2e208130afb2cef6f77663
darrenfishell <darrenfishell@pm.me>
2025-07-23 11:53:47 -0700

    Use agglvl_code for county wages
    More precise specification of agglvl_code here, to get county level totals. This excludes any statewide totals before joining with other sources.
    
    https://www.bls.gov/cew/classifications/aggregation/agg-level-titles.htm

--- dbt_pipeline/models/silver/county_qcew_summary.sql
+++ dbt_pipeline/models/silver/county_qcew_summary.sql
@@ -4,7 +4,7 @@ SELECT
 	oty_annual_avg_emplvl_chg / annual_avg_emplvl as yoy_job_change_pct,
 	ROUND( total_annual_wages / annual_avg_emplvl, 2) as wages_per_job
 FROM {{ source('qcew', 'qcew_annual_average') }}
-WHERE industry_code = '10'
+WHERE agglvl_code = 71
 AND total_annual_wages > 0
 AND own_code = 5
 AND year = 2019
